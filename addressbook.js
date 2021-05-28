87eba653cff5ef003607f07d01332b6476292730
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-28 17:40:09 +0530

    uc4: add existing contact

--- addressbook.js
+++ addressbook.js
@@ -1,3 +1,4 @@
+console.log("Welcome To Addressbook System JavaScript Program");
 const prompt=require('prompt-sync')();
 const NAME_REGEX_PATTERN = RegExp('^[A-Z]{1}[a-z]{2,}$');
 const ADDRESS_REGEX_PATTERN = RegExp('^[a-zA-z]{3,}$');
@@ -67,6 +68,9 @@ class Contact{
             this.email = email;
         }
     }
+    set firstName(firstName){
+        this.firstName = firstName;
+     }
     
     //method
     toString(){
@@ -75,26 +79,83 @@ class Contact{
     }
 }
 
+function findContact(userData){
+    let contactToEdit;
+    for(let i = 0; i < addressBookArray.length; i++){
+        if(addressBookArray[i].firstName === userData)
+            contactToEdit = addressBookArray[i];
+        if(contactToEdit != null){
+            let input = 1;
+            while(input != 9){
+                console.log("\nChoose:");
+                console.log("\n1. Edit First Name \n2. Edit Last Name \n3. Edit Address \n4. Edit City \n5. Edit State");
+                console.log("6. Edit ZipCode \n7. Edit Phone No \n8. Edit Email \n9. View Edited Details & Exit");
+                input = prompt("Enter Your Choice: ");
+                input = parseInt(input);
+                switch (input) {
+                    case 1: let fname = prompt("Enter the firstname: ");
+                            contactToEdit.firstName = fname;
+                            break;
+                    case 2: let lname = prompt("Enter the last Name: ");
+                            contactToEdit.lastName = lname;
+                            break;
+                    case 3: let address_edit = prompt("Enter the address: ");
+                            contactToEdit.address = address_edit;
+                            break;
+                    case 4: let city_edit = prompt("Enter the city: ");
+                            contactToEdit.city = city_edit;
+                            break;
+                    case 5: let state_edit = prompt("Enter the state: ");
+                            contactToEdit.state = state_edit;
+                            break;
+                    case 6: let zip_edit = prompt("Enter the pincode: ");
+                            contactToEdit.zip = zip_edit;
+                            break;
+                    case 7: let phone_edit = prompt("Enter the phone number: ");
+                            contactToEdit.phoneNumber = phone_edit;
+                            break;
+                    case 8: let mail_edit = prompt("Enter the email: ");
+                            contactToEdit.email = mail_edit;
+                            break;
+                    case 9: console.log("\n",contactToEdit);
+                            break;
+                    default: console.log("Wrong Input");
+                    }
+                }
+            }
+    }
+}
+
 //array to store contacts
 let addressBookArray = new Array();
 let countEntry = 0;
 do{
-    countEntry = prompt("Enter 1) Add Contact 0) Exit: ");
+    countEntry = prompt("Press 1) Add Contact 2) Edit Contact 3) View Contact 0) Exit: ");
     if(countEntry == 1){
-    let FirstName = prompt("Enter Firstname: ");
-    let LastName = prompt("Enter Lastname: ");
-    let Address = prompt("Enter Address: ");
-    let City = prompt("Enter City name: ");
-    let State = prompt("Enter State name: ");
-    let Zip = prompt("Enter pincode: ");
-    let PhoneNumber = prompt("Enter phone number: ");
-    let EmailId = prompt("Enter email id: ");
-    try{
-        let person = new Contact(FirstName,LastName,Address,City,State,Zip,PhoneNumber,EmailId);
-        addressBookArray.push(person);
-        console.log("Contact is added: ");
-    }catch(e){
-        console.log(e);
+        let FirstName = prompt("Enter Firstname: ");
+        let LastName = prompt("Enter Lastname: ");
+        let Address = prompt("Enter Address: ");
+        let City = prompt("Enter City name: ");
+        let State = prompt("Enter State name: ");
+        let Zip = prompt("Enter pincode: ");
+        let PhoneNumber = prompt("Enter phone number: ");
+        let EmailId = prompt("Enter email id: ");
+        try{
+            let person = new Contact(FirstName,LastName,Address,City,State,Zip,PhoneNumber,EmailId);
+            addressBookArray.push(person);
+            console.log("Contact is added. ");
+        }catch(e){
+            console.log(e);
+        }
     }
-}
+    if(countEntry == 2){
+        if(addressBookArray.length==0){
+            console.log("No contacts");
+        }
+        let userData = prompt("Enter the contact firstname which you want to edit: ");
+        findContact(userData); 
+    }
+    if(countEntry == 3){
+        console.log(addressBookArray.toString()+"\n");
+    }
 }while(countEntry != 0);
