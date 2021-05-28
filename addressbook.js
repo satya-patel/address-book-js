b89dd5e2d3d0781733f3e8bbc7e554b1e758951d
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-28 16:59:53 +0530

    create addressbook

---
+++ addressbook.js
@@ -0,0 +1,32 @@
+class Contact{
+    //property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    //constructor
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email
+    }
+    
+    //method
+    toString(){
+        return "Firstname: "+this.firstName+" Lastname: "+this.lastName+" Address: "+this.address+" City: "+this.city+
+        " State: "+this.state+" Zip: "+this.zip+" Phone Number: "+this.phoneNumber+" Email: "+this.email;
+    }
+}
+
+let person = new Contact("Mohd","Arshad","Karol Bagh","New Delhi","Delhi",110005,9009009010,"arshad@gmail.com");
+console.log(person.toString());
